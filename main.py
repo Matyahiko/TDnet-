7624319cccf5f6a8a5e3ce6a15825e92b71e119b
matyahiko <sanakan.ver3@gmail.com>
2023-06-03 01:29:34 +0900

    クラスで書き換えた

--- main.py
+++ main.py
@@ -5,124 +5,121 @@ import random
 import os
 from PyPDF2 import PdfFileReader
 import schedule
-import time
 
-MAX_RETRIES = 5
+class TdnetDownloader:
+    def __init__(self, max_retries=5):
+        self.max_retries = max_retries
 
-def get_url(URL):
-    res = requests.get(URL)
-    response_loads = json.loads(res.content)
-    data_list = [data["Tdnet"] for data in response_loads["items"]]
+    def get_url(self, URL):
+        res = requests.get(URL)
+        response_loads = json.loads(res.content)
+        data_list = [data["Tdnet"] for data in response_loads["items"]]
 
-    # pubdate, company_code, document_urlを抽出して辞書にまとめ、リストに格納
-    extracted_data = [
-        {
-            "pubdate": item["pubdate"],
-            "company_code": item["company_code"],
-            "document_url": item["document_url"]
-        } for item in data_list]
+        # pubdate, company_code, document_urlを抽出して辞書にまとめ、リストに格納
+        extracted_data = [
+            {
+                "pubdate": item["pubdate"],
+                "company_code": item["company_code"],
+                "document_url": item["document_url"]
+            } for item in data_list]
 
-    # Save the extracted data
-    with open("path_to_extracted_data.json", "w") as f:
-        json.dump(extracted_data, f, indent=4)
-        
-    return extracted_data
+        # Save the extracted data
+        with open("path_to_extracted_data.json", "w") as f:
+            json.dump(extracted_data, f, indent=4)
 
-def download_pdf(data, max_retries):
-    total = len(data)
-    count = 0
-    failed_downloads = []
+        return extracted_data
 
-    for d in data:
-        if d["company_code"][-1] == "0":
-            retries = 0
-            success = False
+    def download_pdf(self, data):
+        total = len(data)
+        count = 0
+        failed_downloads = []
 
-            while retries < max_retries and not success:
-                try:
-                    pdf = requests.get(d["document_url"])
-                    time.sleep(random.randint(6, 15))
+        for d in data:
+            if d["company_code"][-1] == "0":
+                retries = 0
+                success = False
 
-                    date = d["pubdate"].split(" ")
-                    name = date[0] + "_" + d["company_code"]
-                    file_path = f"row_pdf/{name}.pdf"
+                while retries < self.max_retries and not success:
+                    try:
+                        pdf = requests.get(d["document_url"])
+                        time.sleep(random.randint(6, 15))
 
-                    with open(file_path, "wb") as f:
-                        f.write(pdf.content)
+                        date = d["pubdate"].split(" ")
+                        name = date[0] + "_" + d["company_code"]
+                        file_path = f"row_pdf/{name}.pdf"
 
-                    if validate_pdf(file_path):
-                        success = True
-                        count += 1
-                        progress = int(count / total * 100)
-                        print(f"Progress: {progress}%")
-                    else:
+                        with open(file_path, "wb") as f:
+                            f.write(pdf.content)
+
+                        if self.validate_pdf(file_path):
+                            success = True
+                            count += 1
+                            progress = int(count / total * 100)
+                            print(f"Progress: {progress}%")
+                        else:
+                            retries += 1
+                            os.remove(file_path)  # ダウンロードした壊れたファイルを削除
+                            print(f"Failed to validate {file_path}. Retrying download.")
+
+                    except Exception as e:
+                        print(f"Failed to download {d['document_url']}: {e}")
                         retries += 1
-                        os.remove(file_path)  # ダウンロードした壊れたファイルを削除
-                        print(f"Failed to validate {file_path}. Retrying download.")
 
-                except Exception as e:
-                    print(f"Failed to download {d['document_url']}: {e}")
-                    retries += 1
+                if not success:
+                    failed_downloads.append(d)
 
-            if not success:
-                failed_downloads.append(d)
+        # Save the failed downloads
+        if failed_downloads:
+            with open("path_to_failed_downloads.json", "w") as f:
+                json.dump(failed_downloads, f, indent=4)
 
-    # Save the failed downloads
-    if failed_downloads:
-        with open("path_to_failed_downloads.json", "w") as f:
-            json.dump(failed_downloads, f, indent=4)
+        return failed_downloads
 
-    return failed_downloads
+    def validate_pdf(self, file_path):
+        try:
+            with open(file_path, "rb") as f:
+                pdf = PdfFileReader(f)
+                num_pages = pdf.getNumPages()
+                return num_pages > 0
+        except Exception:
+            return False
 
-def validate_pdf(file_path):
-    try:
-        with open(file_path, "rb") as f:
-            pdf = PdfFileReader(f)
-            num_pages = pdf.getNumPages()
-            return num_pages > 0
-    except Exception:
-        return False
+    def retry_failed_downloads(self):
+        # Load the failed downloads
+        if not os.path.exists("path_to_failed_downloads.json"):
+            print("No failed downloads to retry.")
+            return
 
-def retry_failed_downloads(max_retries):
-    # Load the failed downloads
-    if not os.path.exists("path_to_failed_downloads.json"):
-        print("No failed downloads to retry.")
-        return
+        with open("path_to_failed_downloads.json", "r") as f:
+            failed_downloads = json.load(f)
 
-    with open("path_to_failed_downloads.json", "r") as f:
-        failed_downloads = json.load(f)
+        # Remove the failed downloads file
+        os.remove("path_to_failed_downloads.json")
 
-    # Remove the failed downloads file
-    os.remove("path_to_failed_downloads.json")
+        # Retry the downloads
+        self.download_pdf(failed_downloads)
 
-    # Retry the downloads
-    download_pdf(failed_downloads, max_retries)
+    def run(self):
+        start_date = time.strftime("%Y%m%d", time.localtime(time.time() - 60 * 60 * 24 * 30))
+        end_date = time.strftime("%Y%m%d", time.localtime(time.time()))
 
+        # Set the limit of data to fetch
+        limit = 10000
 
-def run_code():
-    start_date = time.strftime("%Y%m%d", time.localtime(time.time() - 60 * 60 * 24 * 30))
-    end_date = time.strftime("%Y%m%d", time.localtime(time.time()))
-    
-    # Set the limit of data to fetch
-    limit = 10000
+        URL = f"https://webapi.yanoshin.jp/webapi/tdnet/list/{start_date}-{end_date}.json?limit={limit}"
 
-    URL = f"https://webapi.yanoshin.jp/webapi/tdnet/list/{start_date}-{end_date}.json?limit={limit}"
+        # Fetch data from the URL
+        data = self.get_url(URL)
 
-    # Fetch data from the URL
-    data = get_url(URL)
+        # Download PDF files
+        self.download_pdf(data)
 
-    # Set the maximum number of retries
-    max_retries = 5
-        
-    # Download PDF files
-    download_pdf(data, max_retries)
-
-    # Retry failed downloads
-    retry_failed_downloads(max_retries)
+        # Retry failed downloads
+        self.retry_failed_downloads()
 
 
-schedule.every().week.do(run_code)
-
+downloader = TdnetDownloader(max_retries=5)
+schedule.every().week.do(downloader.run)
 
 while True:
     schedule.run_pending()
